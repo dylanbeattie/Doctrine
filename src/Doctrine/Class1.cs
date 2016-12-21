6d1f17eab872148615ef195bc57a1165bca89b92
dylanbeattie <dylan@dylanbeattie.net>
2016-12-21 18:35:46 +0000

    First working demo. Pages, sort order and ToC generator is working.

---
+++ src/Doctrine/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Doctrine {
+    public class DoctrineController : Controller {
+        public ActionResult Index(string path) {
+            var filePath = Server.MapPath("~/Pages/" + path + ".html");
+            var html = HtmlPage.FromPath(filePath);
+            return View("Page", html);
+        }
+    }
+}
---
+++ src/Doctrine/HtmlPage.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace Doctrine {
+    public class HtmlPage {
+        public string Head { get; set; }
+        public string Body { get; set; }
+
+        public static HtmlPage FromPath(string path) {
+            var html = new HtmlDocument();
+            html.Load(path);
+            var headNode = html.DocumentNode.SelectSingleNode("/html/head");
+            var bodyNode = html.DocumentNode.SelectSingleNode("/html/body");
+            var head = headNode == null ? String.Empty : headNode.InnerHtml;
+            var body = bodyNode == null ? String.Empty : bodyNode.InnerHtml;
+            return new HtmlPage() {
+                Head = head,
+                Body = body
+            };
+        }
+    }
+
+    public class TocEntry {
+        public string Sort { get; set; }
+        public string Text { get; set; }
+        public string Href { get; set; }
+        public IList<TocEntry> Children { get; set; }
+    }
+}
